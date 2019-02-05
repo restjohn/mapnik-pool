a3db293b79764516004331d4496cfadd5a1e3f4d
restjohn <restjohn@users.noreply.github.com>
2019-02-05 07:43:52 -0700

    updated tests and test style to test setting the mapnik base dir for resolving relative resources in the style

--- test/pool.js
+++ test/pool.js
@@ -1,6 +1,6 @@
-var test = require('tape').test,
-    mapnik = require('mapnik'),
-    Pool = require('generic-pool').Pool;
+const test = require('tape').test,
+      mapnik = require('mapnik'),
+      genericPool = require('generic-pool');
 
 mapnik.register_default_input_plugins();
 
@@ -8,59 +8,97 @@ var mapnikPool = require('../')(mapnik),
     fs = require('fs');
 
 test('mapnik-pool exposes generic-pool', function(t) {
-    t.equal(Pool,require('../').Pool);
+    t.equal(genericPool, require('../').genericPool);
     t.end();
 });
 
-test('mapnik-pool', function(t) {
-    var pool = mapnikPool.fromString(
+test('from style xml', function(t) {
+    const pool = mapnikPool.fromStyleXML(
             fs.readFileSync(__dirname + '/data/map.xml', 'utf8'),
-            { bufferSize: 256 });
+            { bufferSize: 256 }, { base: __dirname + '/data' });
 
-    pool.acquire(function(err, map) {
-        t.equal(err, null, 'no error returned');
+    pool.acquire().then(map => {
         t.pass('acquires a map');
         t.equal(map.bufferSize, 256, 'sets a buffer size');
         t.equal(map.width, 256, 'sets map size');
         t.equal(map.height, 256, 'sets map size');
-        pool.release(map);
-        pool.drain(function() {
-            pool.destroyAllNow(function() {
-                t.end();
-            });
-        });
+        return map;
+    })
+    .then(map => {
+        return pool.release(map)
+    })
+    .then(() => {
+        return pool.drain();
+    })
+    .then(() => {
+        return pool.clear();
+    })
+    .then(() => {
+        t.end();
+    }, (err) => {
+        t.fail(err);
+    });
+});
+
+test.only('from style file', function(t) {
+    const pool = mapnikPool.fromStylePath(__dirname + '/data/map.xml',
+        { size: 128, bufferSize: 64 });
+
+    pool.acquire().then(map => {
+        t.pass('acquires a map');
+        t.equal(map.bufferSize, 64, 'sets a buffer size');
+        t.equal(map.width, 128, 'sets map size');
+        t.equal(map.height, 128, 'sets map size');
+        return map;
+    })
+    .then(map => {
+        return pool.release(map)
+    })
+    .then(() => {
+        return pool.drain();
+    })
+    .then(() => {
+        return pool.clear();
+    })
+    .then(() => {
+        t.end();
+    }, (err) => {
+        t.fail(err);
     });
 });
 
 test('initOptions', function(t) {
-    var pool = mapnikPool.fromString(
-            fs.readFileSync(__dirname + '/data/map.xml', 'utf8'),
-            { size: 1024 });
+    const pool = mapnikPool.fromStylePath(
+        __dirname + '/data/map.xml', { size: 1024 });
 
-    pool.acquire(function(err, map) {
-        t.equal(err, null, 'no error returned');
+    pool.acquire(map => {
         t.equal(map.width, 1024, 'use initOptions to set map width');
         t.equal(map.height, 1024, 'use initOptions to set map height');
-        pool.release(map);
-        pool.drain(function() {
-            pool.destroyAllNow(function() {
-                t.end();
-            });
-        });
-    });
+        return map;
+    })
+    .then(map => {
+        return pool.release(map);
+    })
+    .then(() => {
+        return pool.drain();
+    })
+    .then(() => {
+        return pool.clear();
+    })
+    .then(t.end, t.fail);
 });
 
-test('passes errors', function(t) {
-    var pool = mapnikPool.fromString('invalid map',
-            { bufferSize: 256 });
+// test('passes errors', function(t) {
+//     const pool = mapnikPool.fromString('invalid map',
+//             { bufferSize: 256 });
 
-    pool.acquire(function(err, map) {
-        t.ok(err instanceof Error,'expected error');
-        t.ok(!map,'expected map to be null');
-        pool.drain(function() {
-            pool.destroyAllNow(function() {
-                t.end();
-            });
-        });
-    });
-});
+//     pool.acquire(function(err, map) {
+//         t.ok(err instanceof Error,'expected error');
+//         t.ok(!map, 'expected map to be null');
+//         pool.drain()
+//         .then(() => {
+//             return pool.clear();
+//         })
+//         .then(t.end, t.fail);
+//     });
+// });
