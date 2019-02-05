086dde929dfaf6296bea45214c8cf2df0034d696
restjohn <restjohn@users.noreply.github.com>
2019-02-05 07:41:08 -0700

    use new generic-pool api and promises; support loading the mapnik style from a file path in addition to a string; set the base dir option on the map when loading from a file

--- index.js
+++ index.js
@@ -1,37 +1,87 @@
-var Pool = require('generic-pool').Pool,
-    os = require('os'),
-    xtend = require('xtend');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const pools = require('generic-pool');
 
-var N_CPUS = os.cpus().length,
-    defaultOptions = { size: 256 },
-    defaultMapOptions = { };
+const N_CPUS = os.cpus().length;
+const defaultOptions = { size: 256 };
+const defaultMapOptions = {};
+
+class Factory {
+
+    constructor(mapnik, style, options, mapOptions) {
+        this.mapnik = mapnik;
+        this.style = style;
+        this.options = Object.freeze(
+            Object.assign({}, defaultOptions, options));
+        this.mapOptions = Object.freeze(
+            Object.assign({}, defaultMapOptions, mapOptions));
+    }
+
+    create() {
+        const map = new this.mapnik.Map(this.options.size, this.options.size);
+        const mapFromString = (xml) => {
+            return new Promise((resolve, reject) => {
+                map.fromString(xml, this.mapOptions, err => {
+                    if (err) {
+                        reject(err);
+                    }
+                    else {
+                        if (this.options.bufferSize) {
+                            map.bufferSize = this.options.bufferSize;
+                        }
+                        resolve(map);
+                    }
+                });
+            });
+        };
+        if (typeof this.style == 'string') {
+            return mapFromString(this.style);
+        }
+        return new Promise((resolve, reject) => {
+            let stylePath = path.resolve(this.style.dir, this.style.name + this.style.ext);
+            fs.readFile(stylePath, 'utf8', (err, content) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(content);
+                }
+            });
+        })
+        .then(mapFromString);
+    }
+
+    destroy(map) {
+        return new Promise((resolve, reject) => {
+            try {
+                map.clear();
+                resolve();
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
 
 module.exports = function(mapnik) {
     return {
-        fromString: function(xml, initOptions, mapOptions) {
-            var options = xtend({}, defaultOptions, initOptions);
+        fromStylePath: function(stylePath, initOptions, mapOptions) {
             mapOptions = mapOptions || {};
-            return Pool({
-                create: create,
-                destroy: destroy,
-                max: N_CPUS
-            });
-            function create(callback) {
-                var map = new mapnik.Map(options.size, options.size);
-                map.fromString(xml, mapOptions, loaded);
-                function loaded(err) {
-                    if (err) return callback(err);
-                    if (options.bufferSize) {
-                        map.bufferSize = options.bufferSize;
-                    }
-                    return callback(err, map);
-                }
+            stylePath = path.parse(path.resolve(stylePath));
+            if (!mapOptions.base) {
+                mapOptions.base = stylePath.dir;
             }
-            function destroy(map) {
-                delete map;
-            }
+            const factory = new Factory(
+                mapnik, stylePath, initOptions, mapOptions);
+            return pools.createPool(factory, { max: N_CPUS });
+        },
+        fromStyleXML: function fromStyleXML(xml, initOptions, mapOptions) {
+            const factory = new Factory(mapnik, xml, initOptions, mapOptions);
+            return pools.createPool(factory, { max: N_CPUS });
         }
     };
 };
 
-module.exports.Pool = Pool;
+module.exports.genericPool = pools;
